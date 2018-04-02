7ba08357c9718f81ea3d0dd423ccee4fcb64a3ff
r5hej <charliebyrdam@gmail.com>
2018-04-02 13:34:32 +0200

    Added empty main activity and set as default

---
+++ MultiplayerAugmentedRealityKit/app/src/main/java/dk/aau/sw805f18/multiplayeraugmentedrealitykit/main/MainActivity.java
@@ -0,0 +1,15 @@
+package dk.aau.sw805f18.multiplayeraugmentedrealitykit.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import dk.aau.sw805f18.multiplayeraugmentedrealitykit.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
