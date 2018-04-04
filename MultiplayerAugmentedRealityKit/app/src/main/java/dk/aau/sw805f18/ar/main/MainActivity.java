5d1b78cf92489fff6f4d7f033d9872c836b40764
timiane <timiane@hotmail.com>
2018-04-04 14:28:28 +0200

    added navbar and ar button to navbar

--- MultiplayerAugmentedRealityKit/app/src/main/java/dk/aau/sw805f18/ar/main/MainActivity.java
+++ MultiplayerAugmentedRealityKit/app/src/main/java/dk/aau/sw805f18/ar/main/MainActivity.java
@@ -2,6 +2,7 @@ package dk.aau.sw805f18.ar.main;
 
 import android.content.Intent;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.NavUtils;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -10,6 +11,7 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import dk.aau.sw805f18.ar.R;
 import dk.aau.sw805f18.ar.ar.ArActivity;
@@ -34,7 +36,14 @@ public class MainActivity extends AppCompatActivity {
                         if(menuItem.isChecked()){
                             menuItem.setChecked(false);
                         }
-                        else menuItem.setChecked(true);
+                        else{
+                            menuItem.setChecked(true);
+                        }
+                        String ar = menuItem.getTitle().toString();
+                        if(ar.equals("AR"))
+                        {
+                            startAr();
+                        }
                         // close drawer when item is tapped
                         mDrawerLayout.closeDrawers();
 
@@ -49,10 +58,9 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         ActionBar actionbar = getSupportActionBar();
-        if (actionbar != null) {
-            actionbar.setDisplayHomeAsUpEnabled(true);
-            actionbar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
-        }
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
+
     }
 
     @Override
@@ -65,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void startAr(View view) {
+    public void startAr() {
         Intent intent = new Intent(this, ArActivity.class);
         startActivity(intent);
     }
