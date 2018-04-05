1a187b4e684038f41337405180446802c2bac84c
Marius Lauge Nørgaard <mariuslauge@gmail.com>
2018-04-05 10:09:11 +0200

    UI refactor.
    Back button functionality, nav drawer changes, main activity refactors.

--- MultiplayerAugmentedRealityKit/app/src/main/java/dk/aau/sw805f18/ar/main/MainActivity.java
+++ MultiplayerAugmentedRealityKit/app/src/main/java/dk/aau/sw805f18/ar/main/MainActivity.java
@@ -2,6 +2,7 @@ package dk.aau.sw805f18.ar.main;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Handler;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
@@ -13,6 +14,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import dk.aau.sw805f18.ar.MainFragment;
 import dk.aau.sw805f18.ar.MapFragment;
@@ -21,7 +23,7 @@ import dk.aau.sw805f18.ar.R;
 import dk.aau.sw805f18.ar.ar.ArActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    private boolean mDoubleBackToExitPressedOnce = false;
     private DrawerLayout mDrawerLayout;
 
     @Override
@@ -30,39 +32,32 @@ public class MainActivity extends AppCompatActivity {
         MainFragment mainFragment = new MainFragment();
 
         setContentView(R.layout.activity_main);
-        FragmentTransaction fragmentTransactor = getSupportFragmentManager().beginTransaction();
-        fragmentTransactor.add(R.id.fragment_container ,mainFragment).commit();
+        FragmentTransaction fragmentTransactorInitial = getSupportFragmentManager().beginTransaction();
+        fragmentTransactorInitial.add(R.id.fragment_container, mainFragment).commit();
         mDrawerLayout = findViewById(R.id.drawer_layout);
 
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(
-                new NavigationView.OnNavigationItemSelectedListener() {
-                    @Override
-                    public boolean onNavigationItemSelected(MenuItem menuItem) {
-                        // set item as selected to persist highlight
-                        FragmentTransaction fragmentTransactorMmap  = getSupportFragmentManager().beginTransaction();
-                        if(menuItem.isChecked()){
-                            menuItem.setChecked(false);
-                        }
-                        else{
-                            menuItem.setChecked(true);
-                        }
-                        switch (menuItem.getTitle().toString()) {
-                            case "AR":
-                                startAr();
-                            case "Map":
-                                MapFragment mapFragment = new MapFragment();
-                                fragmentTransactorMmap.replace(R.id.fragment_container, mapFragment).addToBackStack(null).commit();
-                        }
-                        // close drawer when item is tapped
-                        mDrawerLayout.closeDrawers();
+            new NavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(MenuItem menuItem) {
+                // set item as selected to persist highlight
+                FragmentTransaction fragmentTransactor = getSupportFragmentManager().beginTransaction();
+                menuItem.setChecked(!menuItem.isChecked());
 
-                        // Add code here to update the UI based on the item selected
-                        // For example, swap UI fragments here
+                switch (menuItem.getTitle().toString()) {
+                    case "AR":
+                        startAr();
+                    case "Map":
+                        MapFragment mapFragment = new MapFragment();
+                        fragmentTransactor.replace(R.id.fragment_container, mapFragment).addToBackStack(null);
+                }
 
-                        return true;
-                    }
-                });
+                mDrawerLayout.closeDrawers();
+                fragmentTransactor.commit();
+                return true;
+                }
+            });
 
 
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -71,6 +66,29 @@ public class MainActivity extends AppCompatActivity {
         actionbar.setDisplayHomeAsUpEnabled(true);
         actionbar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
 
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout layout = findViewById(R.id.drawer_layout);
+        if (layout.isDrawerOpen(GravityCompat.START)) {
+            layout.closeDrawer(GravityCompat.START);
+            return;
+        } else if (mDoubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return;
+        }
+
+        this.mDoubleBackToExitPressedOnce = true;
+        Toast.makeText(this, R.string.double_press_back_exit, Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                mDoubleBackToExitPressedOnce = false;
+            }
+        }, 2000);
     }
 
     @Override
