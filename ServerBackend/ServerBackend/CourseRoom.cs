39092ddb962cd26d37adc27994a07300e593d323
rosenbjerg <rosenbjerg@users.noreply.github.com>
2018-04-10 15:28:33 +0200

    added backend server
    currently supports login, opening a room, and joining rooms using websockets

---
+++ ServerBackend/ServerBackend/CourseRoom.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ServerBackend
+{
+    class CourseRoom
+    {
+        public string Owner { get; set; }
+        public string Id { get; set; }
+        
+        private List<string> _players = new List<string>();
+        public IReadOnlyList<string> Players => _players.AsReadOnly();
+
+        public event EventHandler PlayerJoined;
+        public event EventHandler PlayerLeft;
+
+        public void AddPlayer(string player)
+        {
+            _players.Add(player);
+            PlayerJoined?.Invoke(this, EventArgs.Empty);
+        }
+
+
+        public void RemovePlayer(string email)
+        {
+            if (_players.Remove(email))
+                PlayerLeft?.Invoke(this, EventArgs.Empty);
+                
+        }
+    }
+}
---
+++ ServerBackend/ServerBackend/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Net;
+using Newtonsoft.Json;
+using Red;
+using Red.CookieSessions;
+
+namespace ServerBackend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = new RedHttpServer(5000);
+
+            // Temporary "id" generator
+            int roomId = 123;
+
+            var rooms = new ConcurrentDictionary<string, CourseRoom>();
+            
+            server.Use(new CookieSessions(new CookieSessionSettings(TimeSpan.FromDays(14))
+            {
+                Excluded = { "/login" }
+            }));
+            
+            server.Post("/login", async (req, res) =>
+            {
+                var form = await req.GetFormDataAsync();
+                if (form["email"] == "mark" && form["password"] == "mark")
+                    req.OpenSession(form["email"]);
+                else
+                    await res.SendStatus(HttpStatusCode.BadRequest);
+            });
+            
+            server.Post("/openroom", async (req, res) =>
+            {
+                // The logged-in user's email. Used for identification or whatever
+                var email = (string) req.GetSession().Data;
+                
+                var room = new CourseRoom
+                {
+                    Owner = email,
+                    Id = (roomId++).ToString()
+                };
+                rooms[room.Id] = room;
+                await res.SendString(room.Id);
+            });
+            
+            server.WebSocket("/connect/:room", async (req, wsd) =>
+            {
+                if (!rooms.TryGetValue(req.Parameters["room"], out var room))
+                {
+                    await wsd.SendText("No such room! Please consult your leader!");
+                    await wsd.Close();
+                    return;
+                }
+                
+                // The logged-in user's email. Used for identification or whatever
+                var email = (string) req.GetSession().Data;
+                // Function that sends current list of players in room
+                EventHandler playerChangeHandler = delegate { wsd.SendText(JsonConvert.SerializeObject(room.Players)); };
+                // Game leader does not count as player
+                if (email != room.Owner)
+                    room.AddPlayer(email);
+
+                room.PlayerJoined += playerChangeHandler;
+                room.PlayerLeft += playerChangeHandler;
+                
+                // When this websocket connection is closed (for any reason)
+                wsd.OnClosed += delegate
+                {
+                    if (email != room.Owner)
+                        room.RemovePlayer(email);
+
+                    room.PlayerJoined -= playerChangeHandler;
+                    room.PlayerLeft -= playerChangeHandler;
+                };
+            });
+        }
+    }
+}
