b98f71e9f12eb09a80eb34e347d62be8f6996b0c
Jonas Kloster Jacobsen <joklost@gmail.com>
2018-05-11 10:19:23 +0200

    Convert DeviceLocation to singleton

--- MultiplayerAugmentedRealityKit/app/src/main/java/dk/aau/sw805f18/ar/ar/location/LocationScene.java
+++ MultiplayerAugmentedRealityKit/app/src/main/java/dk/aau/sw805f18/ar/ar/location/LocationScene.java
@@ -90,7 +90,7 @@ public class LocationScene {
 
         startCalculationTask();
 
-        deviceLocation = new DeviceLocation(mActivity);
+        deviceLocation = DeviceLocation.getInstance(mActivity);
         deviceOrientation = new DeviceOrientation();
     }
 
