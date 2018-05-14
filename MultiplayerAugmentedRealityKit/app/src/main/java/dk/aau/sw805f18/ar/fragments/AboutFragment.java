2c5fe698d89ce999a03d1017dda4f68bb8f99be3
Marius Lauge Nørgaard <mariuslauge@gmail.com>
2018-05-14 10:00:44 +0200

    Fragment Refactor Commit 2 #19
    Some files didn't get added to previous commit.

---
+++ MultiplayerAugmentedRealityKit/app/src/main/java/dk/aau/sw805f18/ar/fragments/AboutFragment.java
@@ -0,0 +1,28 @@
+package dk.aau.sw805f18.ar.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import dk.aau.sw805f18.ar.R;
+import dk.aau.sw805f18.ar.main.MainActivity;
+
+public class AboutFragment extends Fragment {
+    public static final String TAG_ABOUT = "about";
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_about, container, false);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        MainActivity.CURRENT_FRAGMENT = TAG_ABOUT;
+    }
+}
---
+++ MultiplayerAugmentedRealityKit/app/src/main/java/dk/aau/sw805f18/ar/fragments/StatsFragment.java
@@ -0,0 +1,28 @@
+package dk.aau.sw805f18.ar.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import dk.aau.sw805f18.ar.R;
+import dk.aau.sw805f18.ar.main.MainActivity;
+
+public class StatsFragment extends Fragment {
+    public static final String TAG_STATS = "stats";
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_stats, container, false);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        MainActivity.CURRENT_FRAGMENT = TAG_STATS;
+    }
+}
