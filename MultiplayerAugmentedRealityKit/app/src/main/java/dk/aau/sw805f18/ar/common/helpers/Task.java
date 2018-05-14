e3a9e0c857c2cd62e8f98cfb6805c7a7f56ae852
rosenbjerg <rosenbjerg@users.noreply.github.com>
2018-05-14 11:42:46 +0200

    task

---
+++ MultiplayerAugmentedRealityKit/app/src/main/java/dk/aau/sw805f18/ar/common/helpers/Task.java
@@ -0,0 +1,35 @@
+package dk.aau.sw805f18.ar.common.helpers;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class Task {
+    private static boolean mInit;
+    private static ExecutorService mExecutorService;
+
+
+    public static void run(Runnable runnable) {
+        if (!mInit) {
+            init();
+        }
+        mExecutorService.execute(runnable);
+    }
+
+    private static void init() {
+        int corePoolSize = 3;
+        int maxPoolSize = 10;
+        long keepAliveTime = 5000;
+
+        mExecutorService =
+                new ThreadPoolExecutor(
+                        corePoolSize,
+                        maxPoolSize,
+                        keepAliveTime,
+                        TimeUnit.MILLISECONDS,
+                        new LinkedBlockingQueue<>()
+                );
+        mInit = true;
+    }
+}
