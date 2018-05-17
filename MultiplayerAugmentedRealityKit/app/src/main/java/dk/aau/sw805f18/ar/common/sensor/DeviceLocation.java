08b61edd0b21c2dfafcef2908f12899a9543f1a1
Jonas Kloster Jacobsen <joklost@gmail.com>
2018-05-17 15:44:47 +0200

    Subscribe to received location updates

--- MultiplayerAugmentedRealityKit/app/src/main/java/dk/aau/sw805f18/ar/common/sensor/DeviceLocation.java
+++ MultiplayerAugmentedRealityKit/app/src/main/java/dk/aau/sw805f18/ar/common/sensor/DeviceLocation.java
@@ -2,6 +2,7 @@ package dk.aau.sw805f18.ar.common.sensor;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Context;
 import android.location.Location;
 import android.util.Log;
 
@@ -11,11 +12,16 @@ import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
+
 import dk.aau.sw805f18.ar.common.helpers.ArLocationPermissionHelper;
 
 
 public class DeviceLocation {
-    //    private static final int TWO_MINUTES = 60 * 2 * 1000;
     private static final String TAG = DeviceLocation.class.getSimpleName();
 
     private static DeviceLocation sInstance;
@@ -24,6 +30,9 @@ public class DeviceLocation {
     private Location mCurrentBestLocation;
     private FusedLocationProviderClient mLocationClient;
     private LocationRequest mLocationRequest;
+
+    private Map<Context, Consumer<Location>> mSubscribers;
+
     private LocationCallback mLocationCallback = new LocationCallback() {
         @Override
         public void onLocationResult(LocationResult locationResult) {
@@ -39,6 +48,9 @@ public class DeviceLocation {
 
             if (isBetterLocation(location)) {
                 mCurrentBestLocation = location;
+                for (Consumer<Location> subscriber : mSubscribers.values()) {
+                    subscriber.accept(mCurrentBestLocation);
+                }
             }
         }
     };
@@ -47,6 +59,7 @@ public class DeviceLocation {
     // the Permission Helper.
     @SuppressLint("MissingPermission")
     private DeviceLocation(Activity activity) {
+        mSubscribers = new HashMap<>();
         mLocationClient = LocationServices.getFusedLocationProviderClient(activity);
         mLocationRequest = new LocationRequest();
         mLocationRequest.setInterval(10000);
@@ -63,7 +76,14 @@ public class DeviceLocation {
 
     public Location getCurrentBestLocation() {
         return mCurrentBestLocation;
-//        return BuildLocation(57.01968, 9.988778);
+    }
+
+    public void subscribe(Context context, Consumer<Location> onUpdate) {
+        mSubscribers.put(context, onUpdate);
+    }
+
+    public void unsubscribe(Context context) {
+        mSubscribers.remove(context);
     }
 
     @SuppressLint("MissingPermission")
